25dcb8ea7c7da4ac9bf726149ee9a9840b8f3865
achang <achang@appdynamics.com>
2015-03-05 15:17:05 -0800

    Adding admin css

--- django_base_project/django_base_project/settings/common.py
+++ django_base_project/django_base_project/settings/common.py
@@ -57,11 +57,11 @@ WSGI_APPLICATION = 'django_base_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOSTNAME'],
-        'PORT': os.environ['RDS_PORT'],
+        'NAME': os.environ.get('RDS_DB_NAME'),
+        'USER': os.environ.get('RDS_USERNAME'),
+        'PASSWORD': os.environ.get('RDS_PASSWORD'),
+        'HOST': os.environ.get('RDS_HOSTNAME'),
+        'PORT': os.environ.get('RDS_PORT'),
     }
 }
 
