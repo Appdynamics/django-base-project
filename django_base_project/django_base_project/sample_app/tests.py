f1bc08b973a3de9ccc04d4eb23db7ce11b60c3dd
achang <achang@appdynamics.com>
2015-03-13 17:04:53 -0700

    Appending to the simple selenium test

--- django_base_project/django_base_project/sample_app/tests.py
+++ django_base_project/django_base_project/sample_app/tests.py
@@ -7,15 +7,22 @@ the database, but if your tests interact with the database their behavior will v
 on the order that the test runner executes them. This can lead to unit tests that pass when 
 run in isolation but fail when run in a suite.
 """
+import logging
 import unittest
 
-from django.test import SimpleTestCase, TestCase, Client
+from django.test import Client, SimpleTestCase, TestCase, LiveServerTestCase
 from rest_framework import status
 
-from django.test import LiveServerTestCase
+# http://selenium.googlecode.com/svn/trunk/docs/api/py/index.html
 from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
 
 class SampleBaseTestCase(unittest.TestCase):
+    """
+    Simple test to see if the home page works, implemented as 
+    a unittest.TestCase class.
+    """
+
     def setUp(self):
         # Every test needs a client.
         self.client = Client()
@@ -26,23 +33,28 @@ class SampleBaseTestCase(unittest.TestCase):
 
         # Check that the response is 200 OK.
         self.assertTrue(
-        	status.is_success(response.status_code)
+            status.is_success(response.status_code)
         )
 
 
 class SampleSimpleTestCase(SimpleTestCase):
+    """
+    Simple test to see if the home page works, implemented as
+    a django.test.SimpleTestCase class.
+    """
 
-    def test_home_page(self):
-        """
-        Simple test to see if the home page works.
-        """
-        
+    def test_home_page(self):        
         self.assertTrue(
-        	status.is_success(self.client.get('/').status_code)
+            status.is_success(self.client.get('/').status_code)
         )
 
 
 class SampleSeleniumTests(LiveServerTestCase):
+    """
+    Simple test to see if the home page works, implemented as
+    a django.test.SimpleTestCase class.
+    """
+
     fixtures = ['user-data.json']
 
     @classmethod
@@ -55,10 +67,16 @@ class SampleSeleniumTests(LiveServerTestCase):
         cls.selenium.quit()
         super(SampleSeleniumTests, cls).tearDownClass()
 
-    def test_login(self):
+    def test_admin_login(self):
         self.selenium.get('%s%s' % (self.live_server_url, '/admin/login/'))
+
         username_input = self.selenium.find_element_by_name("username")
-        username_input.send_keys('myuser')
+        username_input.send_keys('username')
         password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys('secret')
+        password_input.send_keys('password')
         self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
+
+        timeout = 2
+        WebDriverWait(self.selenium, timeout).until(
+            lambda driver: driver.find_element_by_tag_name('body')
+        )
