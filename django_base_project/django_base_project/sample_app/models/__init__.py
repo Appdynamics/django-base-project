6eaeac01776b650b2a20d0f4583cf396cba2bc2f
achang <achang@appdynamics.com>
2015-03-16 14:54:37 -0700

    Updating folder tree

---
+++ django_base_project/django_base_project/sample_app/models/__init__.py
@@ -0,0 +1,1 @@
+from sample_app.models.sample_model import *
---
+++ django_base_project/django_base_project/sample_app/models/sample_model.py
@@ -0,0 +1,9 @@
+"""
+A model is the single, definitive source of data about your data. It contains the essential fields
+and behaviors of the data you’re storing. Django follows the DRY Principle. The goal is to define your 
+data model in one place and automatically derive things from it.
+
+This includes the migrations - unlike in Ruby On Rails, for example, migrations are entirely derived 
+from your models file, and are essentially just a history that Django can roll through to update your
+database schema to match your current models.
+"""
