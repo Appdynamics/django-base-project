4356b4faddd77c9fd98cdac0306dabb6bea9dd2d
achang <achang@appdynamics.com>
2015-03-17 14:46:52 -0700

    Adding model mommey examples

--- project_name/project_name/sample_app/models/sample_model.py
+++ project_name/project_name/sample_app/models/sample_model.py
@@ -6,4 +6,39 @@ data model in one place and automatically derive things from it.
 This includes the migrations - unlike in Ruby On Rails, for example, migrations are entirely derived 
 from your models file, and are essentially just a history that Django can roll through to update your
 database schema to match your current models.
+
+In this sample model, we create the same models referenced from model-mommy's documentation so that
+we can show the power of testing with your models via model-mommy.
+
+[1]: http://model-mommy.readthedocs.org/en/stable/basic_usage.html
 """
+
+from django.db import models
+
+class Kid(models.Model):
+    """
+    Model class Kid of family app
+    """
+    happy = models.BooleanField()
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    bio = models.TextField()
+    wanted_games_qtd = models.BigIntegerField()
+    birthday = models.DateField()
+    appointment = models.DateTimeField()
+
+    class Meta:
+        verbose_name = _(u'Kid')
+        verbose_name_plural = _(u'Kids')
+
+    def __unicode__(self):
+        """
+        Retorn the name of kid
+        """
+        return u'%s' % (self.name)
+
+class Dog(models.Model):
+    """
+    Model class Dog of family app
+    """
+    owner = models.ForeignKey('Kid')
---
+++ project_name/project_name/sample_app/tests/test_sample_model.py
@@ -0,0 +1,25 @@
+"""
+[1]: http://model-mommy.readthedocs.org/en/stable/basic_usage.html
+"""
+
+# Core Django imports
+from django.test import TestCase
+
+# Third-party app imports
+from model_mommy import mommy
+from model_mommy.recipe import Recipe, foreign_key
+
+# Relative imports of the 'app-name' package
+from .models import Kid
+
+class KidTestModel(TestCase):
+    """
+    Class to test the model
+    Kid
+    """
+
+    def setUp(self):
+        """
+        Set up all the tests
+        """
+        self.kid = mommy.make(Kid)
