18b2ea3117bcd746326692a455a63cc30600c3cd
achang <achang@appdynamics.com>
2015-03-17 18:09:04 -0700

    Adding more model tests

--- project_name/project_name/sample_app/tests/test_sample_model.py
+++ project_name/project_name/sample_app/tests/test_sample_model.py
@@ -23,3 +23,62 @@ class KidTestModel(TestCase):
         Set up all the tests
         """
         self.kid = mommy.make(Kid)
+
+    def test_creating_multiple_kids(self):
+        kids = mommy.make('family.Kid', _quantity=3)
+        assert len(kids) == 3
+
+class DogTestModel(TestCase):
+    """
+    Class to test the model
+    Dog
+    """
+
+    def setUp(self):
+        """
+        Set up all the tests
+        """
+        self.rex = mommy.make('family.Dog')
+
+
+class KidTestModel(TestCase):
+    """
+    Class to test the model
+    Kid
+    """
+
+    def setUp(self):
+        """
+        Set up all the tests
+        """
+        self.kid = mommy.make(Kid)
+
+    def test_kid_creation(self):
+    	assert self.kid != None
+
+    def test_creating_multiple_kids(self):
+        kids = mommy.make('{{ project_name }}.Kid', _quantity=3)
+        assert len(kids) == 3
+
+class DogTestModel(TestCase):
+    """
+    Class to test the model
+    Dog
+    """
+
+    def setUp(self):
+        """
+        Set up all the tests
+        """
+        self.rex = mommy.make('{{ project_name }}.Dog')
+
+    def test_dog_creation(self):
+    	assert self.rex != None
+
+    def test_creating_multiple_dogs(self):
+        dogs = mommy.make('{{ project_name }}.Dog', _quantity=3)
+        dogs = mommy.make('sample_app.Dog', _quantity=3)
+        assert len(dogs) == 3
+        
+        for dog in dogs:
+            assert dog.owner != None
